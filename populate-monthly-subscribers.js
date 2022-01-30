58d57be564818fa01b6f8f1d29dd42fb1297ef1e
Aamir Adat <74106642+amdat99@users.noreply.github.com>
2022-01-30 11:24:57 +0000

    add email transaction

--- populate-monthly-subscribers.js
+++ populate-monthly-subscribers.js
@@ -1,12 +1,18 @@
 const request = require("request");
 const http = require("http");
+const fs = require("fs");
 const path = require("path");
 const sqlite3 = require("sqlite3");
 const { transporter } = require("./emailConfig");
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 // Documentation for request is available at https://www.npmjs.com/package/request
 // Documentation for sqlite3 is available at https://github.com/mapbox/node-sqlite3/wiki
 // Retrieve a list of users on a monthly plan from the Marketing API, then add rows to the
 // monthly_subscribers table.
+
+const token = "Bearer " + process.env.TOKEN;
+const sql = fs.readFileSync("./create-recipients-view.sql").toString();
+
 const db = new sqlite3.Database("client.db", (err) => {
   if (err) {
     console.error(err.message);
@@ -14,16 +20,12 @@ const db = new sqlite3.Database("client.db", (err) => {
   console.log("Connected to database");
 });
 
-db.run(
-  `CREATE TABLE recipients AS SELECT email FROM newsletters WHERE promotions = 0 ORDER BY email ASC`,
-  [],
-  function (err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    console.log("Recipients table created");
+db.run(sql, [], function (err) {
+  if (err) {
+    return console.log(err.message);
   }
-);
+  console.log("Recipients table created");
+});
 
 let currentUrl;
 
@@ -31,20 +33,38 @@ const server = http.createServer((req, res) => {
   currentUrl = req.url;
   const path = req.url.split("?")[0];
 
-  path === "/subscriptions" &&
+  if (path === "/users") {
     getSubscriptions((response) => {
-      res.write(JSON.stringify(response));
-      res.end();
+      if (response?.result.length) {
+        try {
+          db.serialize(() => {
+            db.run("begin transaction");
+            response.result.forEach((user) => {
+              if (user.hasOwnProperty("subscription")) {
+                db.run(`INSERT INTO monthly_subscribers (email) VALUES (?)`, [user.email]);
+              }
+            });
+            db.run("commit");
+          });
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify({ status: 200, message: "Successfully added subscribed emails" }));
+        } catch (err) {
+          console.error(err);
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify({ status: 500, message: "error adding subscribed emails" }));
+        }
+      }
     });
-
-  path === "/email" &&
+  } else if (path === "/email") {
     sendMail((response) => {
       res.write(JSON.stringify(response));
       res.end();
     });
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Subscriber API");
+  }
 });
-
-const token = process.env.TOKEN;
 
 const getSubscriptions = (response) => {
   const serverUrl = `https://data-engineer-test.xara.com` + currentUrl;
@@ -59,7 +79,7 @@ const getSubscriptions = (response) => {
       Accept: "application/json",
     },
   };
-  request(options, (error, res, body) => {
+  request(options, (error, _res, body) => {
     if (error) {
       console.log(error);
       response(error);
@@ -72,8 +92,8 @@ const sendMail = (response) => {
   const uploadDir = path.dirname(require.main.filename);
   const attachments = [
     {
-      path: uploadDir + "/client.db",
-      filename: "client.db",
+      path: uploadDir + "/create-recipients-view.sql",
+      filename: "create-recipients-view.sql",
     },
     {
       path: uploadDir + "/populate-monthly-subscribers.js",
